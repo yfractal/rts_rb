b8197863a94ac1528b7d19d0fe1da1b193925761
yfractal <yfractal@gmail.com>
2022-05-23 10:37:45 +0800

    init

---
+++ lib/rts_rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rts_rb/version"
+
+module RtsRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rts_rb_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RtsRb do
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
